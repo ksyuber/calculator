54583ff05204c5a22325f5fb52d76b285dd8f00c
Ksenia Berkutova <molloveme94@gmail.com>
2017-04-29 01:10:30 +0400

    Добавлена обработка команды выход.

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			while (true) {
+				System.out.print("> ");
+				String command = input.readLine();
+				
+				if (command.equals("exit")) {
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Exception: " + e.toString());
+		}
 	}
 }
