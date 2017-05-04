3f4eeb86afb19f3f68ea85066d604ce40242ab8e
Ksenia Berkutova <molloveme94@gmail.com>
2017-05-04 23:48:19 +0400

    Добавлены отдельные классы для операций.

--- src/main/java/calculator/Main.java
+++ src/main/java/calculator/Main.java
@@ -7,40 +7,129 @@ import java.security.InvalidParameterException;
 import java.util.*;
 
 public class Main {
+	
+	public interface Operator {
+		public double process(Double arg1, Double arg2);
+		public int priority();
+	}
 
-	// класс оператора, определяет его приоритет и производит вычисления над
-	// аргументами
-	public static class Operator {
-		// сам оператор
-		public char operator;
-		// приоритет оператора. У * и / приоритет выше чем у + и -
-		public int priority;
-
-		// конструктор оператора, принимает символ оператора и определяет его
-		// приоритет
-		public Operator(char op) {
-			operator = op;
-			if (op == '+' || op == '-') {
-				priority = 1;
-			} else if (op == '*' || op == '/') {
-				priority = 2;
-			}
+	public static class OperatorPlus implements Operator {
+		@Override
+		public int priority() {
+			return 1;
 		}
 
-		// вычисляет результат применения оператора над двумя аргументами
+		@Override
 		public double process(Double arg1, Double arg2) {
-			if (operator == '+') {
-				return arg1 + arg2;
-			} else if (operator == '-') {
-				return arg1 - arg2;
-			} else if (operator == '*') {
-				return arg1 * arg2;
-			} else if (operator == '/') {
-				return arg1 / arg2;
-			} else {
-				return 0;
-			}
+			return arg1 + arg2;
 		}
+	}
+	
+	public static class OperatorMinus implements Operator {
+		@Override
+		public int priority() {
+			return 1;
+		}
+		
+		@Override
+		public double process(Double arg1, Double arg2) {
+			return arg1 - arg2;
+		}
+	}
+	
+	public static class OperatorMultiply implements Operator {
+		@Override
+		public int priority() {
+			return 2;
+		}
+		
+		@Override
+		public double process(Double arg1, Double arg2) {
+			return arg1 * arg2;
+		}
+	}
+	
+	public static class OperatorDivide implements Operator {
+		@Override
+		public int priority() {
+			return 2;
+		}
+		
+		@Override
+		public double process(Double arg1, Double arg2) {
+			return arg1 / arg2;
+		}
+	}
+	
+	public static class OperatorBuilder {
+		
+		private char m_operatorSymbol; 
+		
+		public OperatorBuilder symbol(char operatorSymbol) {
+			m_operatorSymbol = operatorSymbol;
+			return this;
+		}
+		
+		public Operator build() {
+			switch (m_operatorSymbol) {
+			case '+':
+				return new OperatorPlus();
+			case '-':
+				return new OperatorMinus();
+			case '*':
+				return new OperatorMultiply();
+			case '/':
+				return new OperatorDivide();
+			default:
+				break;
+			}
+			
+			return null;
+		}
+	}
+	
+//	// класс оператора, определяет его приоритет и производит вычисления над
+//	// аргументами
+//	public static class Operator {
+//		const public char OPERATOR_PLUS = '+';
+//		// сам оператор
+//		public char operator;
+//		// приоритет оператора. У * и / приоритет выше чем у + и -
+//		public int priority;
+//
+//		// конструктор оператора, принимает символ оператора и определяет его
+//		// приоритет
+//		public Operator(char op) {
+//			operator = op;
+//			if (op == OPERATOR_PLUS || op == '-') {
+//				priority = 1;
+//			} else if (op == '*' || op == '/') {
+//				priority = 2;
+//			}
+//		}
+//
+//		// вычисляет результат применения оператора над двумя аргументами
+//		public double process(Double arg1, Double arg2) {
+//			if (operator == '+') {
+//				return arg1 + arg2;
+//			} else if (operator == '-') {
+//				return arg1 - arg2;
+//			} else if (operator == '*') {
+//				return arg1 * arg2;
+//			} else if (operator == '/') {
+//				return arg1 / arg2;
+//			} else {
+//				return 0;
+//			}
+//		}
+//	}
+	
+	private static boolean isNumberSymbol(char symbol) {
+		return symbol >= '0' && symbol <= '9';
+	}
+	
+	private static boolean isOperatorSymbol(char symbol) {
+		return symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/';
 	}
 
 	// метод, вычисляющий значение выражения
@@ -58,18 +147,20 @@ public class Main {
 		for (int i = 0; i < input.length(); ++i) {
 			char currentChar = input.charAt(i);
 
-			boolean isNumberPart = currentChar >= '0' && currentChar <= '9';
-			boolean isOperator = currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/';
+			boolean isOperator = isOperatorSymbol(currentChar);
+			boolean isNumber = isNumberSymbol(currentChar);
 			boolean isEnd = i == input.length() - 1;
 			boolean isNumberEnd = isOperator || currentChar == ' ' || isEnd;
 
-			if (isNumberPart) {
+			if (isNumber) {
 				currentNumber += currentChar;
 			}
 
 			Operator currentOperator = null;
-			if (isOperator) {
-				currentOperator = new Operator(currentChar);
+			if (isOperatorSymbol(currentChar)) {
+				currentOperator = new OperatorBuilder()
+						.symbol(currentChar)
+						.build();
 			}
 
 			if (isNumberEnd && !currentNumber.isEmpty()) {
@@ -79,11 +170,10 @@ public class Main {
 
 			if (isOperator || isEnd) {
 				while (operators.size() > 0 && numbers.size() > 1
-						&& (isEnd || isOperator && operators.peek().priority >= currentOperator.priority)) {
+						&& (isEnd || isOperator && operators.peek().priority() >= currentOperator.priority())) {
 					Double arg2 = numbers.pop();
 					Double arg1 = numbers.pop();
-					Operator lastOperator = operators.pop();
-					numbers.push(lastOperator.process(arg1, arg2));
+					numbers.push(operators.pop().process(arg1, arg2));
 				}
 
 				if (isOperator) {
